20a9086d53e54b322ad7b906d45fda82ef9921db
Austin Bland <45363385+austinbland1@users.noreply.github.com>
2018-11-28 09:17:12 -0600

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,33 @@
+// Austin Bland
+// Programming I
+// 8-29-2018
+// Purpose of Program
+// Print this is your first program to the screen
+
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    string variable;
+    //int
+    //float
+    //double
+    //bool
+    //void
+    //char
+
+
+  cout << ("Hello, what is your name?") << endl;
+  cin >> name; 
+
+  cout << "Hello, " << name << "!" << endl;
+  cout << "What is your favorite food, " << name << "?" << endl;
+  cin >> food;
+  cout << "I like " << food << "too!" << endl;
+
+
+  return 0; //This means the program is DONE
+}
